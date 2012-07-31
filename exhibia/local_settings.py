96922fafba451cf7807f51000f329d4a46a6d053
Victor H. Salazar <exhibia@gmail.com>
2012-07-31 17:32:46 -0400

    fixed settings

--- exhibia/local_settings.py
+++ exhibia/local_settings.py
@@ -1,4 +1,16 @@
 from common_settings import *
+
+DATABASES = {
+    'default': {
+        "ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
+        "NAME": "exhibia.db",                       # Or path to database file if using sqlite3.
+        "USER": "",                             # Not used with sqlite3.
+        "PASSWORD": "",                         # Not used with sqlite3.
+        "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
+        "PORT": "",                             # Set to empty string for default. Not used with sqlite3.
+    },
+}
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
@@ -7,16 +19,5 @@ CACHES = {
         'KEY_PREFIX': 'bidstick'
     }
 }
-
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        'LOCATION': 'unique-snowflake',
-        'TIMEOUT': 60,
-        'KEY_PREFIX': 'bidstick'
-    }
-}
-
 
 
