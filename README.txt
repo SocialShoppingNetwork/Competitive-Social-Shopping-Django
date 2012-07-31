a57f35c0f3e1e577dd9be431f983d31f3b274234
Victor H. Salazar <exhibia@gmail.com>
2012-07-31 17:31:39 -0400

    fixed memcache settings

--- README.txt
+++ README.txt
@@ -4,5 +4,9 @@ ipython
 sqlite3
 python-dev
 libmemcached-dev
+nodejs
+npm socket.io memcached
+
+build-essential
 mkvirtualenv exhibiaenv
 pip install -U pip
--- exhibia/local_settings.py
+++ exhibia/local_settings.py
@@ -1,14 +1,11 @@
 from common_settings import *
-
-DATABASES = {
+CACHES = {
     'default': {
-        "ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        "NAME": "exhibia.db",                       # Or path to database file if using sqlite3.
-        "USER": "",                             # Not used with sqlite3.
-        "PASSWORD": "",                         # Not used with sqlite3.
-        "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
-        "PORT": "",                             # Set to empty string for default. Not used with sqlite3.
-    },
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': ['localhost:11211'],
+        'TIMEOUT': 60,
+        'KEY_PREFIX': 'bidstick'
+    }
 }
 
 
