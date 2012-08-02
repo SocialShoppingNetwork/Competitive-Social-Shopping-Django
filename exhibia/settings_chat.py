176702114afcec5525301bc7ccb61248f9c30415
Victor H. Salazar <exhibia@gmail.com>
2012-08-02 11:26:32 -0400

    added chat app

---
+++ exhibia/settings_chat.py
@@ -0,0 +1,35 @@
+from settings import *
+
+INSTALLED_APPS = [
+    # Django
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.humanize",
+
+    "pinax.templatetags",
+
+    # external
+    "staticfiles",
+    "compressor",
+    "timezones",
+    "pagination",
+    "idios",
+    # Pinax
+    "pinax.apps.account",
+    "gunicorn",
+
+    'django_socketio',
+
+    # project
+    "about",
+    "profiles",
+    "utils",
+    # ndevs apps
+    "socials",
+    "social_auth",
+]
+
