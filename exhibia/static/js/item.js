0dbe4b2f94806f1cf60c3380a85545b2246a1085
jor3l <djjor3l@gmail.com>
2012-08-14 21:08:13 -0500

    Quicksand y scrolling text, need to add some bid items to the database so I can build the sorting, actually, clicking on any of the sorting button group will result in all the items sorted by name

--- exhibia/static/js/item.js
+++ exhibia/static/js/item.js
@@ -10,4 +10,50 @@ $(document).ready(function() {
 			$el.css({'position': 'relative'}); 
 		}
 	});*/
+	
+	/* Quick sand sorting */
+	var $items_container = $('#items');
+	var $items_clone     = $items_container.clone(); // We sort this to do not break the original
+	var $sorting         = $('.btn-group.sorting button');
+		$sorting.click(function() {
+			console.log('sorting by name');
+			var $button = $(this);
+			var $filteredData = $items_clone.find('li'); // Get all the items
+			
+			// Sort by name
+			var $sortedData = $filteredData.sorted({
+				by: function(v) {
+				  return $(v).find('.fund-title').text().toLowerCase();
+				}
+			  });
+			console.log($sortedData);
+			 $items_container.quicksand($sortedData, {
+			  	duration: 800,
+			  	easing: 'easeInOutQuad'
+			 });
+
+		});
 });
+
+// Custom sorting plugin
+(function($) {
+    $.fn.sorted = function(customOptions) {
+        var options = {
+            reversed: false,
+            by: function(a) { return a.text(); }
+        };
+        $.extend(options, customOptions);
+        $data = $(this);
+        arr = $data.get();
+        arr.sort(function(a, b) {
+            var valA = options.by($(a));
+            var valB = options.by($(b));
+            if (options.reversed) {
+                return (valA < valB) ? 1 : (valA > valB) ? -1 : 0;
+            } else {
+                return (valA < valB) ? -1 : (valA > valB) ? 1 : 0;
+            }
+        });
+        return $(arr);
+    };
+})(jQuery);
