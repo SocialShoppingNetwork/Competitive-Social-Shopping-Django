fece6f3a1c704b4f6f20139a118b59359691c3a8
Victor H. Salazar <xtealc@gmail.com>
2012-08-23 19:59:50 -0400

    update settings

--- exhibia/local_settings.py
+++ exhibia/local_settings.py
@@ -2,11 +2,11 @@ from common_settings import *
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        "NAME": "exhibia.db",                       # Or path to database file if using sqlite3.
-        "USER": "",                             # Not used with sqlite3.
+        "ENGINE": "django.db.backends.postgresql_psycopg2", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
+        "NAME": "exhibia",                       # Or path to database file if using sqlite3.
+        "USER": "vh5",                             # Not used with sqlite3.
         "PASSWORD": "",                         # Not used with sqlite3.
-        "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "localhost",                             # Set to empty string for localhost. Not used with sqlite3.
         "PORT": "",                             # Set to empty string for default. Not used with sqlite3.
     },
 }
