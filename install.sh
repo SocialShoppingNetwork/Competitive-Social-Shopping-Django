74c89f1e9f4c2484ad161c5c058584a38c5fe60e
Anssi Herranen <anssi@logoplus.fi>
2014-05-28 13:30:20 +0300

    More fixes to Vagrantfile and install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 sudo apt-get update -y
 sudo apt-get install -y python-software-properties
-sudo add-apt-repository -y ppa:chris-lea/node.js
-sudo npm config set registry http://registry.npmjs.org/
-sudo apt-get update -y
 sudo apt-get install -y vim-nox build-essential git python-pip python-dev screen libpcre3-dev python-software-properties python g++ make nodejs git build-essential vim memcached libmysqlclient-dev libevent-dev libxml2-dev libxslt1-dev libmemcached-dev
+sudo apt-get update -y
 
 sudo pip install virtualenvwrapper
+
+# Node stuff
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo npm config set registry http://registry.npmjs.org/
 
 echo '' >> $HOME/.bashrc
 echo export WORKON_HOME=$HOME/.virtualenvs >> $HOME/.bashrc 
@@ -16,4 +18,3 @@ source /usr/local/bin/virtualenvwrapper.sh
 
 mkvirtualenv exhibiaenv
 workon exhibiaenv
-
